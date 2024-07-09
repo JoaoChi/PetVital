d52d8b4a1b7bdb2d37d2c97b0cf420b3ef7d66d4
João <joao.angellira@gmail.com>
2024-07-09 14:29:48 -0300

    First Commit

---
+++ app/src/main/java/com/angellira/petvital1/CadastroActivity.kt
@@ -0,0 +1,24 @@
+package com.angellira.petvital1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.angellira.petvital1.databinding.ActivityCadastroBinding
+import com.angellira.petvital1.databinding.ActivityEditarPerfilBinding
+import com.angellira.petvital1.databinding.ActivityMainBinding
+
+class CadastroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCadastroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        }
+    }
